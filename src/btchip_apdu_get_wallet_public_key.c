541fdeb476c1b0c78c29723fc01500d17b32e698
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-12-07 16:33:17 +0100

    Update os_global_pin_is_validated check

--- src/btchip_apdu_get_wallet_public_key.c
+++ src/btchip_apdu_get_wallet_public_key.c
@@ -66,7 +66,7 @@ unsigned short btchip_apdu_get_wallet_public_key() {
     }
 
 
-    if (!os_global_pin_is_validated()) {
+    if (os_global_pin_is_validated() != BOLOS_TRUE) {
         return BTCHIP_SW_SECURITY_STATUS_NOT_SATISFIED;
     }
 
